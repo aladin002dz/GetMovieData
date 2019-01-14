f5f4ea89f386f5e527ca690adb657aa1bb9a7923
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-01-14 09:02:06 +0100

    Adding evironment variables

--- src/lambda/getmovie.js
+++ src/lambda/getmovie.js
@@ -10,9 +10,8 @@ export async function handler(event, context) {
     console.log("query params: "+JSON.stringify(params));
     const title = params.title;
     const year = params.year;
-    const API_URL = "https://www.omdbapi.com/";
-    const API_CLIENT_ID =  "xxxx";
-    const API_CLIENT_SECRET = "yyyy";
+
+   const { API_URL, API_CLIENT_ID, API_CLIENT_SECRET } = process.env;
     
     const URL = `${API_URL}?i=${API_CLIENT_ID}&apikey=${API_CLIENT_SECRET}&t=${title}&y=${year}`;
 
