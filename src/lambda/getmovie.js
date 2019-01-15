2a83bf4235108597b5f3f44d241b1c29352a2127
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-01-15 10:31:06 +0100

    updating footer links

--- src/lambda/getmovie.js
+++ src/lambda/getmovie.js
@@ -11,11 +11,7 @@ export async function handler(event, context) {
     const title = params.title;
     const year = params.year;
     
-    const API_URL = "https://www.omdbapi.com/";
-    const API_CLIENT_ID =  "tt3896198";
-    const API_CLIENT_SECRET = "265eb80d";
-
-   //const { API_URL, API_CLIENT_ID, API_CLIENT_SECRET } = process.env;
+    const { API_URL, API_CLIENT_ID, API_CLIENT_SECRET } = process.env;
     
     const URL = `${API_URL}?i=${API_CLIENT_ID}&apikey=${API_CLIENT_SECRET}&t=${title}&y=${year}`;
 
