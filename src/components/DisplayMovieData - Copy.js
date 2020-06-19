7a53a0feba5d0d1992a4f5a83e78e3c3806b89ec
aladin002dz <mahfoudh.arous@gmail.com>
2020-06-19 21:41:22 +0100

    update DisplayMovieData class component to function

---
+++ src/components/DisplayMovieData - Copy.js
@@ -0,0 +1,136 @@
+import React, { Component } from 'react'
+import './DisplayMovieData.css'
+
+//export default class DisplayMovieData extends Component {
+export default function DisplayMovieData {
+
+    render() { 
+
+    try{
+        let { movieData } = this.props;
+
+        if(movieData != null)
+        {
+            let { Released, Genre, Director, Writer,
+            Actors, Plot, Language, Poster, imdbRating, imdbVotes,
+        Ratings, Production} = movieData;
+
+        const ratings = Ratings.map((rating) =>
+            <li><span>{rating.Source}:</span> {rating.Value} </li>
+            );
+        return (
+            <div className="movie-data"> 
+                <div className="row">
+                    <div className="col-sm-12 col-md-7">
+                        <ul className="list-group">
+                            <li className="list-group-item">             
+                                <span>IMDB Rating:</span> { imdbRating }
+                            </li> 
+                            <li className="list-group-item">
+                            <span>IMDB Votes:</span> { imdbVotes }
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Ratings:</span>
+                                <ul style={{ listStyleType: "none"}}>
+                                    { ratings}
+                                </ul>
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Actors:</span> { Actors }
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Plot:</span> { Plot }
+                            </li>  
+                            <li className="list-group-item">
+                            <span>Released:</span> { Released }
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Genre:</span> { Genre }
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Director:</span> { Director }
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Writer:</span> { Writer }
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Language:</span> { Language }
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Production:</span> { Production }
+                            </li> 
+                        </ul>
+                    </div>
+                    <div className="col-sm-12 col-md-5">
+                        <img className="poster" src={Poster} alt="Poster"/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else
+    {
+        return (
+            <div className="movie-data"> 
+                <div className="row">
+                    <div className="col-sm-12 col-md-7">
+                        <ul className="list-group">
+                            <li className="list-group-item">             
+                                <span>IMDB Rating:</span>                             </li> 
+                            <li className="list-group-item">
+                            <span>IMDB Votes:</span> 
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Ratings:</span>
+                                <ul style={{ listStyleType: "none"}}>
+                                    
+                                </ul>
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Actors:</span> 
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Plot:</span> 
+                            </li>  
+                            <li className="list-group-item">
+                            <span>Released:</span>
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Genre:</span> 
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Director:</span> 
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Writer:</span> 
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Language:</span>
+                            </li> 
+                            <li className="list-group-item">
+                            <span>Production:</span> 
+                            </li> 
+                        </ul>
+                    </div>
+                    <div className="col-sm-12 col-md-5">
+                        <div className="empty-poster">
+                                    Movie Poster
+                        </div>
+                    </div>
+                </div>
+            </div>
+                    )
+    }
+
+}
+ catch(err)
+    {
+        return (
+            <div style={{marginLeft:'2rem', paddingLeft:'2rem'}}>
+                {err.message}
+            </div>
+        )
+    }
+
+  }
+}
--- src/lambda/getmovie.js
+++ src/lambda/getmovie.js
@@ -11,7 +11,7 @@ export async function handler(event, context) {
     const year = params.year;
 
     const { API_URL, API_CLIENT_ID, API_CLIENT_SECRET } = process.env;
-    
+
     const URL = `${API_URL}?i=${API_CLIENT_ID}&apikey=${API_CLIENT_SECRET}&t=${title}&y=${year}`;
 
     const response = await fetch(URL);
